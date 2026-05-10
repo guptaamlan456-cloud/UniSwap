d6c49a0ae46c4a7813094b056c38ec70fb6b3f4b
guptaamlan456-cloud <guptaamlan456@gmail.com>
2026-05-10 18:13:08 +0800

    database debug

--- config.py
+++ config.py
@@ -5,16 +5,19 @@ from dotenv import load_dotenv
 basedir = Path(__file__).resolve().parent
 load_dotenv(basedir / ".env")
 
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    print("DATABASE_URL =", os.environ.get("DATABASE_URL"))
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
-    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
-    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+    CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
 
     UPLOAD_FOLDER = "uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
