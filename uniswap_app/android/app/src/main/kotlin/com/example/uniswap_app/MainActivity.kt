96c54d131ba1c0543776864f28edacc604ca4f6f
guptaamlan456-cloud <guptaamlan456@gmail.com>
2026-05-16 15:53:05 +0800

    apk with flutter add

---
+++ uniswap_app/android/app/src/main/kotlin/com/example/uniswap_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uniswap_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ uniswap_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebPage(),
+    );
+  }
+}
+
+class WebPage extends StatefulWidget {
+  const WebPage({super.key});
+
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..enableZoom(true)
+      ..loadRequest(
+        Uri.parse("https://uniswap-production.up.railway.app"),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
